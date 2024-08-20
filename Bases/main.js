a7115ece21d701abdb01deda66c4d119484a4ef9
BrunoAlan <alan.brunoab@gmail.com>
2024-08-20 19:12:18 -0300

    chore: Add for-of.ts to TypeScript bases

--- Bases/main.js
+++ Bases/main.js
@@ -1,30 +1,20 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
 (() => {
-    const avengers = {
-        nick: 'Samuel L Jackson',
-        ironman: 'Robert Downey Jr',
-        vision: 'Paul Bettany',
-        activo: true,
-        poder: 1500
+    const ironman = {
+        name: 'Ironman',
+        weapon: 'Armor Suit'
     };
-    const printAvenger = (_a) => {
-        var { ironman } = _a, rest = __rest(_a, ["ironman"]);
-        console.log(avengers.nick, rest);
+    const thor = {
+        name: 'Thor',
+        weapon: 'Mjolnir'
     };
-    printAvenger(avengers);
-    const avengersArray = ['Thor', 'Spiderman', 'Ironman'];
-    const [thor, ironman] = avengersArray;
-    console.log(thor, ironman);
+    const captainAmerica = {
+        name: 'Captain America',
+        weapon: 'Shield'
+    };
+    const avengers = [ironman, thor, captainAmerica];
+    for (const avenger of avengers) {
+        console.log(avenger);
+    }
 })();
 //# sourceMappingURL=main.js.map
